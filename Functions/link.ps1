446f2a4d99b02a362c040250f84c4d8c0ecee20b
-Z3US- <32334634+cyberlooper@users.noreply.github.com>
2020-05-23 11:08:25 +0100

    Initial headers

---
+++ Functions/link.ps1
@@ -0,0 +1,9 @@
+<###############################################################################################################
+Smoogle Support - Assisted Setup script
+
+v1.0    04th September 2019:    A script is born                               
+
+Written By Justin Looper - Smoogle Support
+
+All Code provided as is and used at your own risk.
+###############################################################################################################>
